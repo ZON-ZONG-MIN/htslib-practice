2499bf908c141036543d1d602b38cbc9bfcdf304
rufus <z92232123@gmail,com>
2023-07-10 17:54:27 +0800

    print contig

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <iostream>
+#include "Phasing.h"
 #include "htslib/vcf.h"
 
 int main(int argc, char *argv[])
@@ -13,8 +16,10 @@ int main(int argc, char *argv[])
 	fp = vcf_open(argv[1], "r");
 	hdr = vcf_hdr_read(fp);
 	ctg = hdr->id[BCF_DT_CTG];
+	//ctg = hdr->id[BCF_DT_SAMPLE];
+	//ctg = hdr->id[BCF_DT_ID];
 	for(i = 0; i < hdr->n[BCF_DT_CTG]; ++i)
-		printf("%s\t%d\n", ctg[i].key, ctg[i].val->info[0]);
+		printf("%s\t%ld\n", ctg[i].key, ctg[i].val->info[0]);
 	bcf_hdr_destroy(hdr);
 	vcf_close(fp);
 	return 0;
--- makefile
+++ makefile
@@ -2,7 +2,7 @@ CC = g++
 
 
 test: test
-		$(CC) main.cpp -o test -L/path/to/htslib/dir -I/path/to/htslib/dir -lhts
+		$(CC) main.cpp -o test	 -L/path/to/htslib/dir -I/path/to/htslib/dir -lhts
 
 clean:
 		rm test
